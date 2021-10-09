0da619061a2a5348791bfee83755379876486fcd
0Smurkles <89548586+0Smurkles@users.noreply.github.com>
2021-10-10 00:07:02 +0100

    Update _whitelist.lua

--- _whitelist.lua
+++ _whitelist.lua
@@ -1,13 +1,18 @@
 local module = {}
 
 module.Whitelisted = {
+	-- Alts
 	['154220234'] = 'ySmurkles',
 	['2515727162'] = 'yHakuuI',
+	['1040765797'] = 'Kick_Tester',
+	
+	-- Cortex Members
 	['672244259'] = 'yPaduu',
 	['2610042393'] = 'h4tyx4',
-	['1040765797'] = 'Kick_Tester',
 	['252479400'] = 'yPoisoned',
 	['2514879625'] = '0nlyJoa0',
+	
+	-- Paid Access
 }
 
 return module
