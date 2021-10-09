2193e008a6bef938f7a857ac52a33b735e3d1465
0Smurkles <89548586+0Smurkles@users.noreply.github.com>
2021-10-09 22:17:36 +0100

    Create _whitelist.lua

---
+++ _whitelist.lua
@@ -0,0 +1,11 @@
+local module = {}
+
+module.Whitelisted = {
+	['154220234'] = 'ySmurkles',
+    ['2515727162'] = 'yHakuuI',
+	['672244259'] = 'yPaduu',
+    ['2610042393'] = 'h4tyx4',
+    ['1040765797'] = 'Kick_Tester',
+}
+
+return module
