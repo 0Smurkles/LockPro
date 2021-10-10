ddd235ab5f24a8f36e06a0290636a5fc3065ec07
0Smurkles <89548586+0Smurkles@users.noreply.github.com>
2021-10-10 14:28:39 +0100

    Update details.lua

--- details.lua
+++ details.lua
@@ -3,14 +3,15 @@ local module = {}
 module.Version = '0.1.0'
 module.Owner = 'LE0#0001'
 
-module.Defaults = {
+module.SaveDefault = [[
+return {
   Radius = 150,
   Distance = 100,
   Ping = 120,
   LockingTo = 'UpperTorso',
   ToggleCircle = Enum.KeyCode.B,
-  ToggleMenu = Enum.KeyCode.M,
-}
+  ToggleMenu = Enum.KeyCode.M,}
+]]
 
 module.LockingParts = {
   'UpperTorso', 'LowerTorso', 'Head'
