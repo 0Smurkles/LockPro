b94b749cb1cfbbe4b058c1112a71adf241632a99
Smurkles <89548586+0Smurkles@users.noreply.github.com>
2021-10-16 22:07:05 +0100

    Update _whitelist.lua

--- _whitelist.lua
+++ _whitelist.lua
@@ -5,6 +5,7 @@ module.Whitelisted = {
 	['154220234'] = 'ySmurkles',
 	['2515727162'] = 'yHakuuI',
 	['1040765797'] = 'Kick_Tester',
+	['2865049250'] = 'Tester_Kick',
 	
 	-- Cortex Members
 	['672244259'] = 'yPaduu',
@@ -13,10 +14,11 @@ module.Whitelisted = {
 	['2514879625'] = '0nlyJoa0',
 	['564938682'] = 'ySkxzx',
 	['563557636'] = 'Thrrxs',
+	['2329561797'] = 'ealtdog',
 	
 	
 	-- Paid Access
-	['2329561797'] = 'ealtdog',
+	
 	
 }
 
